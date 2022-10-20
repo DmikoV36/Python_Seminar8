0fa35811003aefead5c1cf72526998ebcf340e2b
Dmitriy <Dmiko@bk.ru>
2022-10-20 17:15:31 +0300

    Доделал меню

--- main.py
+++ main.py
@@ -11,54 +11,150 @@
 
 from easygui import *
 
-msg = "Выберите дальнейшее действие: "
-title = "Информация об учениках школы № ?"
-choices = ['Добавить информацию об ученике', 'Удалить информацию об ученике', 'Редактировать информацию об ученике', 'Поиск информации']
-choice = choicebox(msg, title, choices)
+lvl = 0
 
-if choice == choices[0]:
-    msg = "Введите информацию об ученике: "
-    title = "Ввод информации об ученике"
-    fieldNames  =  ["ФИО","Дата рождения","Класс","Классный руководитель","Адрес","Телефон родителя","Средняя успеваемость"]
-    fieldValues = []
-    fieldValues = multenterbox(msg, title, fieldNames)
-    while True:
-        if fieldValues == None: break
-        errmsg = ""
-        for i in range (len(fieldNames)-1):
-            if fieldValues[i].strip() == "":
-                errmsg = errmsg + ('"%s" это обязательные поля.\n\n' % fieldNames[i])
-        if errmsg == "":
-            break # no problems found
-        fieldValues = multenterbox(errmsg, title,  fieldNames, fieldValues)
-    print ("Введена инфомация:", fieldValues)
-
-if choice == choices[1]:
-    msg = "Введите ФИО ученика: "
-    title = "Удаление информации об ученике"
-    fieldNames  =  ["ФИО"]
-    fieldValues = []
-    fieldValues = multenterbox(msg, title, fieldNames)
-
-if choice == choices[2]:
-    msg = "Выберите данные для редактирования: "
-    title = "Редактирование данных об ученике"
-    choices = ["ФИО","Дата рождения","Класс","Классный руководитель","Адрес","Телефон родителя","Средняя успеваемость"]
-    overwriting = multchoicebox(msg, title, choices)
-    if choices[3]:
+while True:
+    if lvl == 0:
+        msg = "Выберите дальнейшее действие: "
+        title = "Информация об учениках школы № ?"
+        choices = ['Добавить информацию об ученике', 'Удалить информацию об ученике', 'Редактировать информацию об ученике', 'Поиск информации']
+        choice = choicebox(msg, title, choices)
+        if choice == choices[0]:
+            lvl = 1
+        if choice == choices[1]:
+            msg = "Введите ФИО ученика: "
+            title = "Удаление информации об ученике"
+            fieldNames  =  ["Фамилия","Имя Отчество"]
+            del_stud = []
+            del_stud = multenterbox(msg, title, fieldNames)
+            if del_stud == False:
+                lvl = 0
+            else:
+                pass
+        elif choice == choices[2]:
+            lvl = 2
+        elif choice == choices[3]:
+            lvl = 3
+        elif choice == None:
+            msgbox("Работа системы завершена. До новых встреч!")
+            break
+    elif lvl == 1:
         msg = "Введите информацию об ученике: "
         title = "Ввод информации об ученике"
-        fieldNames  =  overwriting
-        fieldValues = []
-        fieldValues = multenterbox(msg, title, fieldNames)
-
-if choice == choices[3]:
-    msg = "Выберите шаблон поиска: "
-    title = "Поиск информации"
-    choices = ['Информация об ученике', 'Классный руководитель ученика', 'Телефон родителя', 'Ученики класса', 'Именинники месяца', 'Выборка по успеваемости']
-    choice = choicebox(msg, title, choices)
-    if choice == choices[5]:
+        fieldNames  =  ["Фамилия","Имя Отчество","Дата рождения","Класс","Классный руководитель","Адрес","Телефон родителя","Средняя успеваемость"]
+        ent_stud = []
+        ent_stud = multenterbox(msg, title, fieldNames)
+        while True:
+            if ent_stud == None:
+                lvl = 0
+                break
+            errmsg = ""
+            for i in range (len(fieldNames)-1):
+                if ent_stud[i].strip() == "":
+                     errmsg = errmsg + ('"%s" это обязательные поля.\n\n' % fieldNames[i])
+            if errmsg == "":
+                break # no problems found
+            ent_stud = multenterbox(errmsg, title,  fieldNames, ent_stud)
+        print (f"Инфомация: {ent_stud} добавлена в систему.")
+        lvl = 0
+    elif lvl == 2:
+        msg = "Выберите данные для редактирования: "
+        title = "Редактирование данных об ученике"
+        choices = ["Фамилия","Имя Отчество","Дата рождения","Класс","Классный руководитель","Адрес","Телефон родителя","Средняя успеваемость"]
+        overwriting = multchoicebox(msg, title, choices)
+        if overwriting == None:
+            lvl = 0
+        elif choices[3]:
+            msg = "Введите информацию об ученике: "
+            title = "Ввод информации об ученике"
+            #fieldNames  =  overwriting
+            edit_stud = []
+            edit_stud = multenterbox(msg, title, overwriting)
+            print(overwriting)
+            if edit_stud == False:
+                lvl = 2
+            else:
+                print(edit_stud)
+    elif lvl == 3:
         msg = "Выберите шаблон поиска: "
-        title = "Успеваемость"
+        title = "Поиск информации"
+        choices = ['Информация об ученике', 'Классный руководитель ученика', 'Телефон родителя', 'Ученики класса', 'Именинники месяца', 'Выборка по успеваемости']
+        chois_sample = choicebox(msg, title, choices)
+        if chois_sample == choices[0]:
+            lvl = 4
+        elif chois_sample == choices[1]:
+            lvl = 5
+        elif chois_sample == choices[2]:
+            lvl = 6
+        elif chois_sample == choices[3]:
+            lvl = 7
+        elif chois_sample == choices[4]:
+            lvl = 8
+        elif chois_sample == choices[5]:
+            lvl = 9
+        elif chois_sample == None:
+            lvl = 0
+        # elif choice == choices[1]:
+    elif lvl == 4:
+        msg = "Введите ФИО ученика: "
+        title = "Информация об ученике"
+        fieldNames  =  ["Фамилия","Имя Отчество"]
+        sample1 = []
+        sample1 = multenterbox(msg, title, fieldNames)
+        if sample1 == None:
+            lvl = 3
+        else:
+            pass
+    elif lvl == 5:
+        msg = "Введите ФИО ученика: "
+        title = "Классный руководитель ученика"
+        fieldNames  =  ["Фамилия","Имя Отчество"]
+        sample2 = []
+        sample2 = multenterbox(msg, title, fieldNames)
+        if sample2 == None:
+            lvl = 3
+        else:
+            pass
+    elif lvl == 6:
+        msg = "Введите ФИО ученика: "
+        title = "Телефон родителя"
+        fieldNames  =  ["Фамилия","Имя Отчество"]
+        sample3 = []
+        sample3 = multenterbox(msg, title, fieldNames)
+        if sample3 == None:
+            lvl = 3
+        else:
+            pass
+    elif lvl == 7:
+        msg = "Введите класс: "
+        title = "Ученики класса"
+        fieldNames  =  ["Класс"]
+        sample4 = []
+        sample4 = multenterbox(msg, title, fieldNames)
+        if sample4 == None:
+            lvl = 3
+        else:
+            pass
+    elif lvl == 8:
+        msg = "Введите месяц: "
+        title = "Именинники месяца"
+        fieldNames  =  ["Месяц"]
+        sample5 = []
+        sample5 = multenterbox(msg, title, fieldNames)
+        if sample5 == None:
+            lvl = 3
+        else:
+            pass
+    elif lvl == 9:
+        msg = "Выберите категорию: "
+        title = "Выборка по успеваемости"
         choices = ['Отличники', 'Хорошисты', 'Троечники']
-        choice = choicebox(msg, title, choices)
+        score = choicebox(msg, title, choices)
+        if score == None:
+            lvl = 3
+        elif score == choices[0]:
+            pass
+        elif score == choices[1]:
+            pass
+        elif score == choices[2]:
+            pass
